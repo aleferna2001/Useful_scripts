dadccd92d6caaf941542aee71e50c985072c6ba6
aleferna2001 <40150512+aleferna2001@users.noreply.github.com>
2018-07-03 22:09:26 -0300

    Delete elements_0_to_"n".py

--- elements_0_to_"n".py
+++
@@ -1,9 +0,0 @@
-while True:
-    l=[]
-    n=int(input('Quais são os números de 0 até "n"? Digite "n": '))
-    for i in range(n+1):
-        l.append(i)
-    print()
-    print("São eles:")
-    print(l)
-    print()
